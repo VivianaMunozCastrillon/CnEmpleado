1b091f7ae91eaad667fb24145647a64cd6ae307e
VivianaMuñozCastrillon <vivi84castri@gmail.com>
2023-02-28 20:32:01 -0500

    Agregar archivos de proyecto.

---
+++ CnEmpleado/ClsEmpleado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnEmpleado
+{
+    internal class ClsEmpleado
+    {
+        //variables
+        private string nombre;
+        private double sueldo;
+
+        //Metodos
+        public void CargarDatos()
+        {
+            Console.WriteLine("Por favor digite su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Por favor digite su sueldo");
+            sueldo = double.Parse(Console.ReadLine());
+
+        }
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine(nombre + " " + "su sueldo es " + " " + sueldo);
+        }
+
+        public void PagarImpuestos()
+        {
+            if (sueldo > 3000)
+            {
+                Console.WriteLine("Debe pagar impuestos");
+            }
+            else
+            {
+                Console.WriteLine("No debe pagar impuestos");
+            }
+        }
+    }
+}
---
+++ CnEmpleado/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnEmpleado
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Nombre:Viviana Muñoz
+        /// Fecha: 26/02/2023
+        /// Descripcion: Este programa solicita el nombre y sueldo de un empleado e imprime los datos ingresados y si debe pagar impuestos.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //Instancia de clase
+            ClsEmpleado persona = new ClsEmpleado();
+
+            //Llamada de metodos
+            persona.CargarDatos();
+            persona.ImprimirDatos();
+            persona.PagarImpuestos();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
